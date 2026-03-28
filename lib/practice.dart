d2454c102f9636696bf92d65236c24aad77b1028
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2026-03-28 17:21:11 +0500

    Table Generator and Fruit Filter created using Loop

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,40 +1,82 @@
 import 'dart:io';
 
 void main() {
-  print("Hello World!");
-  var myClass = operatorPractice();
-  stdout.writeln('Enter First Number:');
-  int num1 = int.parse(stdin.readLineSync()!);
-  stdout.writeln('Enter Second Number:');
-  int num2 = int.parse(stdin.readLineSync()!);
+  var practice = LoopPractice();
 
-  stdout.writeln(
-    'Enter the operation (addition,subtraction,Multiply,Divide,Modulus) :',
-  );
-  String operation = stdin.readLineSync()!;
-  var Result = myClass.performOperation(operation, num1, num2);
-  print('Result: $Result');
+  // Table Generator
+  practice.generateTable();
+
+  // Fruit name filter (name contain a letters)
+  practice.fruitFilter();
 }
 
-class operatorPractice {
-  dynamic performOperation(String operation, int num1, int num2) {
-    operation = operation.toLowerCase();
-    if (operation == 'plus' || operation == 'addition') {
-      int sum = num1 + num2;
-      return sum;
-    } else if (operation == 'subtract' || operation == 'minus') {
-      int sum = num1 - num2;
-      return sum;
-    } else if (operation == 'multiply' || operation == 'multiplication') {
-      int sum = num1 * num2;
-      return sum;
-    } else if (operation == 'modulus') {
-      int sum = num1 % num2;
-      return sum;
-    } else if (operation == 'divide') {
-      int sum = num1 ~/ num2;
-      return sum;
+class LoopPractice {
+  void generateTable() {
+    print(' ---- Table Generator ----- ');
+    stdout.writeln('Enter the table number:');
+    int number = int.parse(stdin.readLineSync()!);
+    stdout.writeln('Enter the limit:');
+    int limit = int.parse(stdin.readLineSync()!);
+    for (var i = 1; i <= limit; i++) {
+      print('$number x $i = ${number * i}');
     }
-    return 'Error! Operation dosenot exist';
+  }
+
+  void fruitFilter() {
+    var fruitNames = [
+      'Apple',
+      'Mango',
+      'Berry',
+      'Kiwi',
+      'Banana',
+      'Fig',
+      'Orange',
+    ];
+    for (var fruit in fruitNames) {
+      if (fruit.toLowerCase().contains('a')) {
+        print('Names of fruit containing letter a: $fruit');
+      }
+    }
   }
 }
+
+//    ************************ Object Oriented Programming *************************
+
+// void main() {
+//   print("Hello World!");
+//   var myClass = operatorPractice();
+//   stdout.writeln('Enter First Number:');
+//   int num1 = int.parse(stdin.readLineSync()!);
+//   stdout.writeln('Enter Second Number:');
+//   int num2 = int.parse(stdin.readLineSync()!);
+
+//   stdout.writeln(
+//     'Enter the operation (addition,subtraction,Multiply,Divide,Modulus) :',
+//   );
+//   String operation = stdin.readLineSync()!;
+//   var Result = myClass.performOperation(operation, num1, num2);
+//   print('Result: $Result');
+// }
+
+// class operatorPractice {
+//   dynamic performOperation(String operation, int num1, int num2) {
+//     operation = operation.toLowerCase();
+//     if (operation == 'plus' || operation == 'addition') {
+//       int sum = num1 + num2;
+//       return sum;
+//     } else if (operation == 'subtract' || operation == 'minus') {
+//       int sum = num1 - num2;
+//       return sum;
+//     } else if (operation == 'multiply' || operation == 'multiplication') {
+//       int sum = num1 * num2;
+//       return sum;
+//     } else if (operation == 'modulus') {
+//       int sum = num1 % num2;
+//       return sum;
+//     } else if (operation == 'divide') {
+//       int sum = num1 ~/ num2;
+//       return sum;
+//     }
+//     return 'Error! Operation dosenot exist';
+//   }
+// }
